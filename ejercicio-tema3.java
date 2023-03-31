eb4aca7ef8413c95c6eec368b850c4530d6540f6
SymonCL <129554865+SymonCL@users.noreply.github.com>
2023-03-31 16:14:17 -0500

    Add files via upload

---
+++ ejercicio-tema3.java
@@ -0,0 +1,11 @@
+Public class Sumar {
+    public static int suma(int a, int b, int c) {
+        int Sumar = a + b + c;
+        return suma;
+    }
+}
+public static void main(String[] args) {
+    
+    int resultado = sumarNumeros(2, 5, 7);
+    System.out.println(resultado); 
+}
